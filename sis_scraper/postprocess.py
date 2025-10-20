ccf9081dcfe951193d99297400cb7f865a35d50e
Raymond Chen <42894676+ramonechen@users.noreply.github.com>
2025-10-20 11:12:43 -0400

    Rename scraper_data_processing to postprocess

--- sis_scraper/postprocess.py
+++ sis_scraper/postprocess.py
@@ -240,7 +240,7 @@ def main(
             / "generated_instructor_rcsid_name_map.json"
         )
         logging.info(
-            f"Writing generated instructor RCSID name map to {generated_map_path}"
+            f"Writing {len(generated_instructor_rcsid_name_map)} generated instructor RCSID mappings to {generated_map_path}"
         )
         with generated_map_path.open("w", encoding="utf-8") as f:
             json.dump(

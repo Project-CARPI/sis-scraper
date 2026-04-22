064a1bac15aaa188036ccb4e28fcbe8bbbe4bed3
Raymond <42894676+ramonechen@users.noreply.github.com>
2026-04-21 23:31:56 -0400

    Log cron activity temporarily instead of persistently
    Now that we're switching to Docker-based dev and production environments, it's better to have Docker capture and preserve logs than to rely on ephemeral container storage.

--- cron_job.sh
+++ cron_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # --- CONFIGURATION ---
-LOGS_DIR="scraper_logs"
-LOG_FILE="$LOGS_DIR/$(date +%Y%m%d_%H%M%S).log"
+TEMP_LOG="/tmp/current_run.log"
 SERVER_NAME=$(hostname)
 
-SIS_SCRAPER_DIR="scraper/sis_scraper"
 PYTHON_PATH="python3"
+SIS_SCRAPER_DIR="scraper/sis_scraper"
+
 SCRAPER_COMMAND="$PYTHON_PATH $SIS_SCRAPER_DIR/main.py scrape 1998 $(date +%Y)"
 POSTPROCESS_COMMAND="$PYTHON_PATH $SIS_SCRAPER_DIR/main.py postprocess"
 COMMIT_DB_COMMAND="$PYTHON_PATH $SIS_SCRAPER_DIR/main.py commitdb"
@@ -31,14 +31,14 @@ run_pipeline() {
 mkdir -p $LOGS_DIR
 
 # Run scraper, output to stdout and save to a log file
-run_pipeline 2>&1 | tee "$LOG_FILE"
+run_pipeline 2>&1 | tee "$TEMP_LOG"
 
-# Capture exit status of last command
-EXIT_STATUS=$?
+# Capture exit status of run_pipeline
+EXIT_STATUS=${PIPESTATUS[0]}
 
 # --- CHECK FOR FAILURE ---
 if [ $EXIT_STATUS -ne 0 ]; then
-    ERROR_PREVIEW=$(tail -n 5 "$LOG_FILE" | sed 's/"/\\"/g' | tr -d '\n')
+    ERROR_PREVIEW=$(tail -n 5 "$TEMP_LOG" | sed 's/"/\\"/g' | tr -d '\n')
 
     PAYLOAD=$(cat <<EOF
 {
